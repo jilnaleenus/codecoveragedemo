d8793a220bcf63408ab580f10ec1791136b35be3
jilnaleenus <108727632+jilnaleenus@users.noreply.github.com>
2022-07-22 21:14:27 +0530

    Create B.h

---
+++ libB/inc/B.h
@@ -0,0 +1,15 @@
+#include "Common.h"
+void TestB(string s);
+class Child2:public Parent{
+        public:
+                int mul;
+                int divd;
+        public:
+            
+                void getmul();
+		void getdiv();
+	private:
+	
+	void display();
+		
+};
